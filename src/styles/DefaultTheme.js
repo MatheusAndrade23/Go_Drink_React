08e5c0769b3064593a0cc2624b591ba45a638009
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-04-26 17:43:01 -0300

    Initial Config

---
+++ src/styles/DefaultTheme.js
@@ -0,0 +1,35 @@
+export const DefaultTheme = {
+  colors: {
+    primaryColor: '#',
+    secondaryColor: '#',
+    thirdColor: '#',
+  },
+  fonts: {
+    family: {
+      default: "'Open Sans', sans-serif",
+    },
+    sizes: {
+      xsmall: '',
+      small: '',
+      medium: '',
+      large: '',
+      xlarge: '',
+      xxlarge: '',
+      huge: '',
+      xhuge: '',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+  spacings: {
+    xsmall: '',
+    small: '',
+    medium: '',
+    large: '',
+    xlarge: '',
+    xxlarge: '',
+    huge: '',
+    xhuge: '',
+  },
+};
