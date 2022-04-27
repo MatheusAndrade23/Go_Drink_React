64f22bc17e0b03e5fc6c6fdba67fc842ff67af51
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-04-27 11:25:42 -0300

    Themes & Storybook Config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,32 @@
+import { ThemeProvider } from '../src/providers/ThemeProvider';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
-  },
-}
+  // },
+  // backgrounds: {
+  //   default: 'light',
+  //   values: [
+  //     {
+  //       name: 'light',
+  //       value: '#EEEEEE',
+  //     },
+  //     {
+  //       name: 'dark',
+  //       value: '#222831',
+  //     },
+  //   ],
+  // },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider>
+      <Story />
+    </ThemeProvider>
+  ),
+];
