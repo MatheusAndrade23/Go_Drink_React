b1f9881ae97ac7c60eb875a9c525b906ff6fe177
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-04-29 12:30:06 -0300

    Update index.jsx

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,9 +1,13 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
+import { useState, useEffect } from 'react';
+
 import { Header } from '../../components/Header';
 
 export const Home = () => {
+  useEffect(() => {}, []);
+
   return (
     <Styled.HomeContainer>
       <Header />
