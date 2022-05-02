5510d617d12d1833286fd507080b4995f133e4fd
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-05-01 21:53:29 -0300

    Home

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,37 +1,50 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import { Header } from '../../components/Header';
 import { DrinkComponent } from '../../components/DrinkComponent';
+import { ButtonComponent } from '../../components/ButtonComponent';
 import { Loading } from '../../components/Loading';
 
-export const Home = () => {
+export const Home = ({ which = 'alcoholic', index = 'a' }) => {
+  const { name } = useParams();
+  const navigate = useNavigate();
+  const DRINKS_PER_PAGE = 8;
+
   const [drinks, setDrinks] = useState([]);
   const [loadingControl, setLoadingControl] = useState(true);
-
+  const [loadMoreControl, setLoadMoreControl] = useState(DRINKS_PER_PAGE);
   const [drinksToShow, setDrinksToShow] = useState([]);
-  const [next, setNext] = useState(8);
+  const [next, setNext] = useState(0);
 
-  const handleShowMoreDrinks = () => {};
+  const handleShowMoreDrinks = () => {
+    const nextPage = next + DRINKS_PER_PAGE;
+    const nextDrinks = drinks.slice(nextPage, nextPage + DRINKS_PER_PAGE);
+    setDrinksToShow([...drinksToShow, ...nextDrinks]);
+    setNext(nextPage);
+    setLoadMoreControl((loaded) => loaded + DRINKS_PER_PAGE);
+  };
 
   useEffect(() => {
-    const GetData = async () => {
+    const loadData = async () => {
       try {
         const resp = await fetch(
-          'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka',
+          `https://www.thecocktaildb.com/api/json/v1/1/filter.php?${index}=${which}`,
         );
-        const drinks = await resp.json();
-        setDrinks(drinks.drinks);
+        const data = await resp.json();
+        const drinks = data.drinks.reverse();
+        setDrinks(drinks);
+        setDrinksToShow(drinks.slice(0, DRINKS_PER_PAGE));
         setLoadingControl(false);
-      } catch (err) {
-        console.log(err);
+      } catch (error) {
+        console.log(error);
       }
     };
-
-    GetData();
-  }, []);
+    loadData();
+  }, [index, which]);
 
   return (
     <>
@@ -39,16 +52,26 @@ export const Home = () => {
       <Styled.HomeContainer>
         {!loadingControl ? (
           <Styled.DrinksContainer>
-            {drinks.map((drink) => {
-              console.log(drink);
-              return <DrinkComponent drink={drink} key={drink.idDrink} />;
-            })}
+            {drinksToShow.map((drink) => (
+              <DrinkComponent drink={drink} key={drink.idDrink} />
+            ))}
           </Styled.DrinksContainer>
         ) : (
-          <Loading />
+          <Styled.DrinksContainer>
+            <Loading />
+          </Styled.DrinksContainer>
         )}
-        <button onClick={handleShowMoreDrinks}>Load more</button>
+        {loadMoreControl < drinks.length && (
+          <ButtonComponent handleSubmit={handleShowMoreDrinks} bold={false}>
+            Load More
+          </ButtonComponent>
+        )}
       </Styled.HomeContainer>
     </>
   );
+};
+
+Home.propTypes = {
+  which: P.string,
+  index: P.string,
 };
--- src/templates/Home/styles.js
+++ src/templates/Home/styles.js
@@ -4,8 +4,18 @@ export const HomeContainer = styled.section`
   ${({ theme }) => css`
     width: 100%;
     background-color: ${theme.colors.fourthColor};
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & button {
+      width: 150px;
+      margin-bottom: 20px;
+    }
   `}
 `;
+
 export const DrinksContainer = styled.div`
   ${({ theme }) => css`
     padding: ${theme.spacings.small};
@@ -13,5 +23,6 @@ export const DrinksContainer = styled.div`
     display: flex;
     flex-flow: row wrap;
     justify-content: space-evenly;
+    min-height: 100vh;
   `}
 `;
