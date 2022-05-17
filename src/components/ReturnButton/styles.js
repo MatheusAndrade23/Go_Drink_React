8f2084648223a097c09c07054fddd28db797f69c
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-05-17 19:08:43 -0300

    Update styles.js

--- src/components/ReturnButton/styles.js
+++ src/components/ReturnButton/styles.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const ReturnLink = styled.a`
   ${({ theme }) => css`
-    position: absolute;
+    position: fixed;
     color: ${theme.colors.thirdColor};
     top: 10px;
     left: 0px;
