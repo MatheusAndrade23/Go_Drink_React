dfbff428a7dfbfceb485c973df70fa145eac1f37
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-05-31 20:33:05 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,5 @@
 export default {
-  url: '',
   siteName: 'GoDrink',
-  linkApi: '',
-  linkedinUrl: 'https://www.linkedin.com/in/matheus-andrade23',
-  gitHubUrl: 'https://github.com/MatheusAndrade23/GoDrink',
-  apiUrl: 'https://www.thecocktaildb.com/api.php',
-  api2Url: 'http://localhost:5000',
+  dbUrl: 'https://www.thecocktaildb.com',
+  baseUrl: 'http://localhost:5000',
 };
