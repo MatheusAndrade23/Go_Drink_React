82190cb464cb2235914a2660753e67f8987070a0
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-06-02 18:52:08 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,12 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes as Switch,
-  Route,
-} from 'react-router-dom';
+
+import { Routes } from './routes';
 
 import { createRoot } from 'react-dom/client';
-
-import { ThemeProvider } from './providers/ThemeProvider';
-import { AuthProvider } from './providers/AuthProvider';
-
-import { Footer } from './components/Footer';
-
-import { Home } from './templates/Home';
-import { Kinds } from './templates/Kinds';
-import { Drink } from './templates/Drink';
-import { Search } from './templates/Search';
-import { Lists } from './templates/Lists';
-import { Login } from './templates/Login';
-import { AllDrinks } from './templates/AllDrinks';
-import { Favorites } from './templates/Favorites';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
-        <Router>
-          <Switch>
-            <Route path="/list/:index/:list" element={<Lists />} />
-            <Route path="/search/:search" element={<Search />} />
-            <Route path="/login/:action" element={<Login />} />
-            <Route path="/kind/:kind" element={<Kinds />} />
-            <Route path="/drink/:id" element={<Drink />} />
-            <Route path="/all-drinks" element={<AllDrinks />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route exact path="/" element={<Home />} />
-          </Switch>
-        </Router>
-        <Footer />
-      </ThemeProvider>
-    </AuthProvider>
+    <Routes />
   </React.StrictMode>,
 );
