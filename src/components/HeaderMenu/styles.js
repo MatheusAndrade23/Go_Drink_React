4eafa5572539e6b6ddca401adf8d94461731f6b2
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-06-04 21:57:27 -0300

    Carousel

--- src/components/HeaderMenu/styles.js
+++ src/components/HeaderMenu/styles.js
@@ -5,10 +5,50 @@ export const Menu = styled.ul`
     width: 100%;
     border-bottom: 2px solid ${theme.colors.primaryColor};
     background-color: ${theme.colors.secondaryColor};
-    padding: ${theme.spacings.xsmall} ${theme.spacings.huge};
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-around;
+
+    & div.rec-carousel-wrapper{
+      padding: ${theme.spacings.small};
+      align-items: center;
+    }
+
+    & button.rec-arrow {
+      background-color: ${theme.colors.primaryColor} !important;
+      color: ${theme.colors.fourthColor} !important;
+      display: none;
+
+      &:disabled{
+        cursor: pointer;
+        visibility: hidden;
+      }
+
+      &:hover{
+        background-color: ${theme.colors.thirdColor} !important;
+      }
+
+      @media ${theme.media.mediumScreen} {
+        display: block;
+      }
+    }
+
+    & button.rec-dot {
+
+      &.rec-dot_active {
+        background-color: ${theme.colors.thirdColor} !important;
+        box-shadow: 0 0 1px 3px ${theme.colors.thirdColor} !important;
+      }
+
+      &:focus{
+        background-color: ${theme.colors.thirdColor} !important;
+        box-shadow: 0 0 1px 3px ${theme.colors.thirdColor} !important;
+      }
+
+      &:hover{
+        box-shadow: 0 0 1px 3px ${theme.colors.thirdColor} !important;
+      }
+    }
   `}
 `;
