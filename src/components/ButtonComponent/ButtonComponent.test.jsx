2d877a817347b7870765a1f8f777e56651e3fa59
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-06-14 20:23:19 -0300

    Fixing testes

--- src/components/ButtonComponent/ButtonComponent.test.jsx
+++ src/components/ButtonComponent/ButtonComponent.test.jsx
@@ -4,9 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import { ButtonComponent } from '.';
 import { DarkTheme as theme } from '../../styles/theme/DarkTheme';
 
+const mockFunction = () => {};
+
 describe('<ButtonComponent />', () => {
   it('should render with default values', () => {
-    renderTheme(<ButtonComponent>Text</ButtonComponent>);
+    renderTheme(
+      <ButtonComponent handleSubmit={mockFunction}>Text</ButtonComponent>,
+    );
     const buttonComponent = screen.getByRole('button', {
       name: 'Text',
     });
@@ -21,7 +25,9 @@ describe('<ButtonComponent />', () => {
 
   it('should render correct ButtonComponent sizes', () => {
     const { rerender } = renderTheme(
-      <ButtonComponent size="medium">Text</ButtonComponent>,
+      <ButtonComponent size="medium" handleSubmit={mockFunction}>
+        Text
+      </ButtonComponent>,
     );
     const buttonComponent = screen.getByRole('button', {
       name: 'Text',
@@ -33,23 +39,71 @@ describe('<ButtonComponent />', () => {
 
     rerender(
       <ThemeProvider theme={theme}>
-        <ButtonComponent size="xsmall">Text</ButtonComponent>
+        <ButtonComponent size="xsmall" handleSubmit={mockFunction}>
+          Text
+        </ButtonComponent>
       </ThemeProvider>,
     );
 
     expect(screen.getByRole('button', { name: 'Text' })).toHaveStyle({
       'font-size': theme.fonts.sizes.xsmall,
     });
+
+    rerender(
+      <ThemeProvider theme={theme}>
+        <ButtonComponent size="small" handleSubmit={mockFunction}>
+          Text
+        </ButtonComponent>
+      </ThemeProvider>,
+    );
+
+    expect(screen.getByRole('button', { name: 'Text' })).toHaveStyle({
+      'font-size': theme.fonts.sizes.small,
+    });
   });
 
   it('should render with uppercase letters', () => {
-    renderTheme(<ButtonComponent uppercase={true}>Text</ButtonComponent>);
+    renderTheme(
+      <ButtonComponent uppercase={true} handleSubmit={mockFunction}>
+        Text
+      </ButtonComponent>,
+    );
     const buttonComponent = screen.getByRole('button', {
       name: 'Text',
     });
 
     expect(buttonComponent).toHaveStyle({
       'text-transform': 'uppercase',
+    });
+  });
+
+  it('should render with normal weight letters', () => {
+    renderTheme(
+      <ButtonComponent bold={false} handleSubmit={mockFunction}>
+        Text
+      </ButtonComponent>,
+    );
+    const buttonComponent = screen.getByRole('button', {
+      name: 'Text',
+    });
+
+    expect(buttonComponent).toHaveStyle({
+      'font-weight': 'bold',
+    });
+  });
+
+  it('should render the right model', () => {
+    renderTheme(
+      <ButtonComponent model="icon" handleSubmit={mockFunction}>
+        Text
+      </ButtonComponent>,
+    );
+    const buttonComponent = screen.getByRole('button', {
+      name: 'Text',
+    });
+
+    expect(buttonComponent).toHaveStyle({
+      'border-radius': '50%',
     });
   });
 });
