aaa80e412f3e49154bb30b6a84299145d01062cb
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-06-16 16:19:02 -0300

    Page 404

--- src/routes.jsx
+++ src/routes.jsx
@@ -17,6 +17,7 @@ import { Lists } from './templates/Lists';
 import { Login } from './templates/Login';
 import { AllDrinks } from './templates/AllDrinks';
 import { Favorites } from './templates/Favorites';
+import { PageNotFound } from './templates/PageNotFound';
 
 export const Routes = () => {
   return (
@@ -31,7 +32,9 @@ export const Routes = () => {
             <Route path="/drink/:id" element={<Drink />} />
             <Route path="/all-drinks" element={<AllDrinks />} />
             <Route path="/favorites" element={<Favorites />} />
+            <Route exact path="/not-found" element={<PageNotFound />} />
             <Route exact path="/" element={<Home />} />
+            <Route exact path="*" element={<PageNotFound />} />
           </Switch>
           <Footer />
         </AuthProvider>
