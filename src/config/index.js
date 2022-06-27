eca66218c482a7d358737b18718c620b00b1c7c8
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-06-27 15:24:56 -0300

    Revert "Update index.js"
    This reverts commit 635e696a80013498aa55d0908f2d98b6edc397de.

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
   siteName: 'Go Drink',
   dbUrl: 'https://www.thecocktaildb.com',
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://go-drink-backend.vercel.app/',
   linkedin: 'https://www.linkedin.com/in/matheus-andrade23/',
   gitHub: 'https://github.com/MatheusAndrade23',
   programaThor: 'https://programathor.com.br/users/131799',
